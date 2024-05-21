656f53187dd302088838464b94fccfd6ece5e939
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2024-05-21 23:41:14 +0100

    port update

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ app.post('/api/persons', (req, res)=>{
 
 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>{
     console.log(`server listening at port ${PORT}`);
 })
